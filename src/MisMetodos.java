fe2fcebf33f9f327ab5dcf6515a2c94cf77a30f6
David Vilan Miguens <dvilanmiguens@danielcastelao.org>
2021-02-12 09:10:15 +0100

    Initial commit

---
+++ src/MisMetodos.java
@@ -0,0 +1,13 @@
+/**
+ * Contiene metodos utiles para usar en los proyectos
+ */
+public class MisMetodos {
+    /**
+     * Método que monta un saludo
+     * @param mensaje saludo que quiero mostrar
+     * @return retorno una cadena con el mensaje y concatenado con otra string
+     */
+    public static String saludo(String mensaje){
+        return "Saludo: "+mensaje;
+    }
+}
